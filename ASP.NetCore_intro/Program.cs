9133d8c7bfc17d1d0cd275f3cfd870884171a36a
Rajufullstack04 <rajufullstack777@gmail.com>
2026-01-02 09:17:38 +0530

    basicAuth

--- ASP.NetCore_intro/Program.cs
+++ ASP.NetCore_intro/Program.cs
@@ -1,20 +1,23 @@
+
+using ASP.NetCore_intro.Middleware;
+using ASP.NetCore_intro.Services1;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton<IAuthenticateService, AuthenticationService>();
+
 
 var app = builder.Build();
 
+app.UseMiddleware<AuthenticationMiddelware>();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
---
+++ ASP.NetCore_intro/Services1/IAuthenticateService.cs
@@ -0,0 +1,21 @@
+﻿namespace ASP.NetCore_intro.Services1
+{
+    public interface IAuthenticateService
+    {
+
+
+        //Token generation method
+        string GenerateToken(string username, string password);
+        bool ValidateToken(string token);
+
+
+
+        //User validation method
+        bool ValidateUser(string username, string password);
+
+
+        bool ValidaToken(string token);
+
+
+    }
+}
